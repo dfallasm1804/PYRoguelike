d1dc19b0c0f28c2756fce27b96f515b3f1d3b869
Daniel F <dfallasm1804@gmail.com>
2023-06-24 14:23:04 -0600

    up to part 4, field of view implementation and fog of war

--- engine.py
+++ engine.py
@@ -2,6 +2,7 @@ from typing import Set, Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
+from tcod.map import compute_fov
 
 from entity import Entity
 from game_map import GameMap
@@ -13,6 +14,7 @@ class Engine:
         self.event_handler = event_handler
         self.game_map = game_map
         self.player = player
+        self.update_fov()
 
   def handle_events(self, events: Iterable[Any]) -> None:
     for event in events:
@@ -23,12 +25,27 @@ class Engine:
 
       action.perform(self, self.player)
 
+      self.update_fov() #update fov before next player action
+
+  def update_fov(self) -> None:
+    """Recompute the visible area based on the players point of view."""
+
+    self.game_map.visible[:] = compute_fov(
+      self.game_map.tiles["transparent"], # transparency, any non 0 value is transparent
+      (self.player.x, self.player.y), #pov, point to draw FOV
+      radius=8 #fov extension
+    )
+    # If tile is visible, add to explored
+    self.game_map.explored |= self.game_map.visible
+
   def render(self, console: Console, context: Context) -> None:
 
     self.game_map.render(console)
 
     for entity in self.entities:
-      console.print(entity.x, entity.y, entity.char, fg=entity.color)
+      #only print visible entities
+      if self.game_map.visible[entity.x, entity.y]:
+        console.print(entity.x, entity.y, entity.char, entity.color)
 
     context.present(console)
 
